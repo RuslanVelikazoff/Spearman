e3c9af2f47b3ab54a6dc123cd0e3e584f6578315
RuslanVelikazoff <123361452+RuslanVelikazoff@users.noreply.github.com>
2024-07-30 10:33:00 +0300

    Create game

---
+++ Assets/Scripts/WallMover.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMover : MonoBehaviour
+{
+    public float t;
+    void Update()
+    {
+        float y = Mathf.PingPong(Time.time * t, 8);
+
+        transform.position = new Vector3(transform.position.x, y, -0.28f);
+    }
+}
